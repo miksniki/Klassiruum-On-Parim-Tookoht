7b3a0beced4170ee857e25faaa6eb2ce21069bd5
nikitamikson <nikita.mikson@izall.com>
2026-09-07 21:08:53 +0300

    intro text edit

--- src/components/CampaignIntro.tsx
+++ src/components/CampaignIntro.tsx
@@ -5,7 +5,7 @@ import { Icon } from './Icon';
 export function CampaignIntro() {
   return <section className="campaign-intro" aria-labelledby="campaign-title">
     <div className="campaign-date"><span/> ÕPETAJANÄDAL · 5.–11. OKTOOBER</div>
-    <h1 id="campaign-title">Klassiruum on<br/> <span>parim töökoht<span className="title-period">.</span></span></h1>
+    <h1 id="campaign-title">Klassiruum on<br/> <span>parim töökoht</span></h1>
     <p className="campaign-subtitle">Klassiruum on parim töökoht, aga õpetamise kirg vajab tuge, et kesta ja kasvada</p>
 
     <a className="campaign-scroll" href="#fotostuudio">
